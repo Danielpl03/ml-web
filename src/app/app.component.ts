70107279abcc99e80368a357bb6b11c8ca77dcdc
Daniel_pl <danielpineirolago@gmail.com>
2024-10-14 02:49:42 -0400

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from "./core/components/footer/footer.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'webML-app';
