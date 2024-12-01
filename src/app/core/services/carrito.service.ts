f1d36219368eaea5503ce4b55dc146c7b31b1a22
Daniel_pl <danielpineirolago@gmail.com>
2024-12-01 16:32:45 -0500

    Nuevo header con contador en carrito

--- src/app/core/services/carrito.service.ts
+++ src/app/core/services/carrito.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, WritableSignal, inject, signal } from '@angular/core';
+import { Injectable, WritableSignal, computed, inject, signal } from '@angular/core';
 import { Carrito, ItemCarrito } from '../interfaces/carrito';
 import { Moneda, Producto } from '../interfaces/producto';
 import { ProductosService } from './productos.service';
@@ -6,55 +6,62 @@ import { ProductosService } from './productos.service';
 @Injectable({
   providedIn: 'root'
 })
-export class CarritoService{
+export class CarritoService {
 
   constructor() {
-    const cart = localStorage.getItem('cart');
-    if(cart){
-      this.carrito.set( JSON.parse(cart) );
-      if(this.carrito().moneda == undefined)this.clear();
-      this.moneda.set(this.carrito().moneda)
-    }else{
-      this.productsService.getMoneda(1).then( moneda => {
-        this.moneda.set(moneda);
-        this.carrito().moneda = moneda;
-      });
+    if (typeof window !== 'undefined') {
+
+      const cart = localStorage.getItem('cart');
+      if (cart) {
+        this.carrito.set(JSON.parse(cart));
+        if (this.carrito().moneda == undefined) this.clear();
+        this.moneda.set(this.carrito().moneda)
+        this.items.set(this.carrito().items.length);
+      } else {
+        this.productsService.getMoneda(1).then(moneda => {
+          this.moneda.set(moneda);
+          this.carrito().moneda = moneda;
+        });
+      }
     }
-   }
+  }
 
 
-   productsService = inject(ProductosService);
-   moneda:WritableSignal<Moneda | undefined> = signal(undefined);
-
+  productsService = inject(ProductosService);
+  moneda: WritableSignal<Moneda | undefined> = signal(undefined);
+  
   carrito: WritableSignal<Carrito> = signal({
     id: 0,
     moneda: undefined,
     items: []
   });
+  
+  items:WritableSignal< number> = signal(0);
 
-  actualizarImporte(moneda: Moneda){
+  actualizarImporte(moneda: Moneda) {
     this.moneda.set(moneda);
-    this.carrito().items.forEach( item => {
-      item.importe = Math.round(item.cantidad * this.getPrecio(item.producto, moneda) *10) /10 ;
+    this.carrito().items.forEach(item => {
+      item.importe = Math.round(item.cantidad * this.getPrecio(item.producto, moneda) * 10) / 10;
     });
     this.carrito().moneda = moneda;
     this.actualizarLS();
+    
   }
 
-   getPrecio(producto: Producto, moneda: Moneda|undefined){
+  getPrecio(producto: Producto, moneda: Moneda | undefined) {
     if (moneda == undefined) return 0;
-    if(moneda.idMoneda == 1){
+    if (moneda.idMoneda == 1) {
       return producto.precio.precio;
-    }else{
+    } else {
       let precio = 0;
       for (let i = 0; i < producto.precios.length; i++) {
         const precioP = producto.precios[i];
-        if  (precioP.idMoneda == moneda.idMoneda){
+        if (precioP.idMoneda == moneda.idMoneda) {
           precio = precioP.precio;
         }
       }
-      if  (precio == 0){
-        precio = Math.round( (producto.precio.precio / moneda.tazaCambio) * 10 ) / 10 ;
+      if (precio == 0) {
+        precio = Math.round((producto.precio.precio / moneda.tazaCambio) * 10) / 10;
       }
       return precio;
 
@@ -66,7 +73,7 @@ export class CarritoService{
       this.carrito.set({ id: 1, moneda: this.moneda(), items: [] });
     }
     let found = false;
-    
+
     const precio = this.getPrecio(producto, this.moneda())
 
 
@@ -74,10 +81,10 @@ export class CarritoService{
       if (item.producto.idProducto == producto.idProducto) {
         if (cant) {
           item.cantidad = cant;
-          item.importe = Math.round( (cant * precio) * 10 ) / 10 ;
+          item.importe = Math.round((cant * precio) * 10) / 10;
         } else {
           item.cantidad += 1;
-          item.importe = Math.round( (item.cantidad * precio) * 10 ) / 10 ;
+          item.importe = Math.round((item.cantidad * precio) * 10) / 10;
         }
         found = true;
       }
@@ -91,7 +98,7 @@ export class CarritoService{
       this.carrito().items.push(item);
     }
     this.actualizarLS();
-    
+
   }
 
   eliminarDeCarrito(producto: Producto, cant?: number) {
@@ -102,10 +109,10 @@ export class CarritoService{
       if (item.producto.idProducto == producto.idProducto) {
         if (cant != undefined && cant >= 0) {
           item.cantidad = cant;
-          item.importe = Math.round( (cant * precio) * 10 ) / 10 ;
+          item.importe = Math.round((cant * precio) * 10) / 10;
         } else {
           item.cantidad -= 1;
-          item.importe = Math.round( (item.cantidad * precio) * 10 ) / 10 ;
+          item.importe = Math.round((item.cantidad * precio) * 10) / 10;
         }
         if (item.cantidad <= 0) {
           this.carrito().items.splice(this.carrito().items.indexOf(item), 1);
@@ -120,16 +127,16 @@ export class CarritoService{
     return this.carrito().items;
   }
 
-  getImporte(){
+  getImporte() {
     let importe = 0;
-    this.carrito().items.forEach( (item) => {
+    this.carrito().items.forEach((item) => {
       importe += item.importe;
     });
     return Math.ceil(importe);
   }
 
-  clear(){
-    this.carrito.set( {id:0, moneda: this.moneda(), items:[]} );
+  clear() {
+    this.carrito.set({ id: 0, moneda: this.moneda(), items: [] });
     this.actualizarLS();
   }
 
@@ -139,6 +146,7 @@ export class CarritoService{
   }
 
   actualizarLS() {
+    this.items.set(this.carrito().items.length);
     if (this.isEmpty()) {
       localStorage.removeItem('cart')
     } else {
